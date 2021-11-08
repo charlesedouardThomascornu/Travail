591ef616805b4f73bb29d35208dd55755a7b7316
charlesedouardThomascornu <92926066+charlesedouardThomascornu@users.noreply.github.com>
2021-11-08 09:49:31 +0100

    Add files via upload

---
+++ Les_Instructions_Conditionnelles.py
@@ -0,0 +1,21 @@
+# Excercice 1
+#moyenne >= 10
+#nomFille == 'Alice'
+#ageIndividu < 18
+#moyenneBac >= 12
+
+# Excercice 2.1
+def bac(prenom, nom, moyenne):
+    if moyenne >= 10:
+        print(prenom, nom, "a obtenu son bac, Félicitations!")
+        
+# Excercice 2.2
+def bac2(prenom, nom, moyenne):
+    if moyenne >= 10:
+        print(prenom, nom, "a obtenu son bac, Félicitations!")
+    else:
+        print(prénom, nom, "n'a pas obtenu son bac!")
+
+
+        
+        
